416b9466cf1935945d3a1d15906434f2dfb29a51
João Pedro Xavier Freitas <pxfjoaopedro@gmail.com>
2024-06-30 16:51:03 -0300

    Atualizando git

--- desenvolvimento-front-end-avancado/aula-3/front/src/App.js
+++ desenvolvimento-front-end-avancado/aula-3/front/src/App.js
@@ -1,9 +1,18 @@
-import Products from './pages/Products'
+import Home from "./pages/Home"
+import Esportista from "./pages/Esportista"
+import Treino from "./pages/Treino"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 export default function App() {
   return (
     <div>
-      <Products />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/esportista/:id" element={<Esportista/>}></Route>
+          {/* <Route path="/treino" elemetn={<Treino/>}></Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
